b4ef443fabe6db0d4f471369dff1b5d91f78599a
Ziad Hassanein <ziadmuhammed422@outlook.com>
2025-02-20 23:14:04 +0200

    Add project files.

---
+++ MVCapp1/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCapp1.Models;
+
+namespace MVCapp1.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductSamples productSamples = new();
+        #region actios test
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //public ViewResult Test()
+        //{
+        //    ViewResult result = new()
+        //    {
+        //        ViewName = "view1"
+        //    };
+        //    return result;
+
+        //}
+        //public JsonResult tesst()
+        //{
+        //    //return Json(new { Id = 1, name = "test" });
+        //    JsonResult result = new(new { Id = 1, name = "test" });
+        //    return result;
+        //}
+        //public IActionResult index(int id)
+        //{
+        //    if (id % 2 == 0)
+        //    {
+        //        return Content("ok");
+        //    }
+        //    else
+        //    {
+        //        return View("view1");
+        //    }
+        //} 
+        #endregion
+        public IActionResult GetProduct(int id)
+        {
+            Product ProductModel = productSamples.GetByID(id);
+            //productSamples.GetAll();
+            return View("Products", ProductModel);
+        }
+        public IActionResult GetAllProducts()
+        {
+            List<Product> Products = productSamples.GetAll();
+            return View("GetAllProducts", Products);
+        }
+    }
+}
---
+++ MVCapp1/Models/ProductSamples.cs
@@ -0,0 +1,20 @@
+﻿namespace MVCapp1.Models
+{
+    public class ProductSamples
+    {
+        readonly List<Product> products;
+        public ProductSamples()
+        {
+            products =
+            [
+                new Product { ID = 1, Name = "Mercedes 1", Price = 100, Description = "CLE Coupe", Image = "1.jpg" },
+                new Product { ID = 2, Name = "Mercedes 2", Price = 200, Description = "AMG GT 2025", Image = "2.jpg" },
+                new Product { ID = 3, Name = "Mercedes 3", Price = 300, Description = "GLE Class 2025", Image = "3.jpg" },
+            ];
+        }
+        public List<Product> GetAll() => products ?? [];
+ 
+        public Product GetByID(int id) => products?.FirstOrDefault(p => p.ID == id) ?? throw new KeyNotFoundException($"Product with ID {id} not found.");
+
+    }
+}
